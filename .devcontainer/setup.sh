852a1a90c7e600e25e632f69b3f2e5c57b0adc4a
AH-StyleSeat <92470703+AH-StyleSeat@users.noreply.github.com>
2023-04-13 17:03:49 -0400

    initial

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --login
+rvm install 2.7.4 
+rvm use 2.7.4 --default
+sudo apt-add-repository -y -s "deb http://security.ubuntu.com/ubuntu bionic-security main"
+sudo apt-get update --allow-insecure-repositories
+apt-cache policy libssl1.0-dev
+sudo apt-get install --allow-unauthenticated -y libssl1.0-dev
+rvm install 2.2
+curl https://www.openssl.org/source/openssl-1.0.2l.tar.gz | tar xz && cd openssl-1.0.2l && sudo ./config && sudo make && sudo make install
+sudo ln -sf /usr/local/ssl/bin/openssl `which openssl`
+openssl version -v
+rvm install 2.3.8 --with-openssl-dir=/usr/bin/openssl
+gem install bundler -v=1.17.3
+sudo apt-get -y install redis
+sudo service redis-server start
+sudo apt-get install build-essential
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/home/linuxbrew/.linuxbrew/bin/brew install mysql@5.7
+/home/linuxbrew/.linuxbrew/bin/brew services start mysql@5.7
